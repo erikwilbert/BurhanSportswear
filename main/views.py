21987778f53038ff77b439fd3a345bc15d3c9c03
erikwilbert <erik.wilbert@ui.ac.id>
2025-09-03 17:00:03 +0700

    Completed Assignments - 1

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'nama_aplikasi' : 'Burhan Sportswear',
+        'name': 'Erik Wilbert',
+        'class': 'PBP B'
     }
 
     return render(request, "main.html", context)
