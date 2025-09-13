f4c880a79f355cea8beecb2de66751b5cec015dd
erikwilbert <erik.wilbert@ui.ac.id>
2025-09-13 20:22:01 +0700

    Program completed

--- burhansportswear/settings.py
+++ burhansportswear/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "erik-wilbert-burhansportswear.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = ["https://erik-wilbert-burhansportswear.pbp.cs.ui.ac.id"]
 
 # Application definition
 
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'burhansportswear.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
